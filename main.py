b946ac191f01b5862de0f499a2509c61dffcec22
TheHudat <jhudakjh@gmail.com>
2025-06-12 14:18:50 -0400

    Created stats.py to hold functions

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+from stats import word_count
+
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
-
-def word_count(text):
-    return len(text.split())
 
 def main():
     num_words = word_count(get_book_text("/home/jhudak/workspace/github.com/TheHudat/bookbot/books/frankenstein.txt"))
