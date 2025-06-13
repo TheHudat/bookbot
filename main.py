abcc109b9a6527f4cc70bbc3eeb9a46eaf51a41b
TheHudat <jhudakjh@gmail.com>
2025-06-13 10:58:44 -0400

    Created character_count function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from stats import word_count
+from stats import word_count, character_count
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
         return f.read()
 
 def main():
-    num_words = word_count(get_book_text("/home/jhudak/workspace/github.com/TheHudat/bookbot/books/frankenstein.txt"))
+    book_text = get_book_text("/home/jhudak/workspace/github.com/TheHudat/bookbot/books/frankenstein.txt")
+    num_words = word_count(book_text)
     print (f"{num_words} words found in the document")
+    character_count(book_text)
 
 main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def word_count(text):
     return len(text.split())
+
+def character_count(text):
+    lower_case_text = text.lower()
+    character_dict = {}
+    for c in lower_case_text:
+        if c in character_dict:
+            character_dict[c] += 1
+        else: 
+            character_dict[c] = 1
+    print (character_dict)
